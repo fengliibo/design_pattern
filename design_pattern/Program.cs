f914c884e1071fbcf34c9b0bd2900569431398fa
nick <nfeng@movoto.com>
2017-12-22 10:09:06 +0800

    Singleton instance

---
+++ design_pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design_Pattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ design_pattern/singletonInstance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Design_Pattern
+{
+    public class singletonInstance
+    {
+        private static singletonInstance instance = null;
+        private static readonly object threadSafeLock = new object();
+        public static singletonInstance Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (threadSafeLock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new singletonInstance();
+                        }
+
+                    }
+                }
+                return instance;
+            }
+
+        }
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void Hi()
+        {
+            Console.WriteLine("Hi");
+        }
+        public singletonInstance()
+        {
+        }
+    }
+}
